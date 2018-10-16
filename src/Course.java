7724e5070a663cf6fc4402fdfb4737721419b5b8
Osman <osman.afandiyev@mail.utoronto.ca>
2018-10-16 07:31:36 +0400

    abstract class UnknownP is created and Course extends that class

--- src/Course.java
+++ src/Course.java
@@ -2,42 +2,17 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class Course implements UnknownP, Iterable{
-
-    private String name;
-    private ArrayList<ArrayList<Event>> lstOfSections = new ArrayList<>();
-
+public class Course extends UnknownP{
     public Course(String name) {
         this.name = name;
     }
-    public void add(ArrayList<Event> Section) {
-        lstOfSections.add(Section);
-    }
-    public String getCoursename() {
-        return this.name;
-    }
-    public Iterator iterator() {
-        return new SectionIterator();
-    }
-
     @Override
     public boolean equals(Object obj) {
         Course alma = (Course) obj;
-        return this.getCoursename().equals(alma.getCoursename());
+        return this.getName().equals(alma.getName());
     }
 
-    public class SectionIterator implements Iterator{
-        int cuurentSection = 0;
-        @Override
-        public boolean hasNext(){
-            return cuurentSection + 1 < lstOfSections.size();
-        }
-        @Override
-        public List<Event> next() {
-//            TODO implement DEEPclone
-            return lstOfSections.get(cuurentSection++);
-        }
-    }
+
 
 
 
--- src/UnknownP.java
+++ src/UnknownP.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Currently this is the superClass for Course. We want it to represent anything which behaves like course, in the
  * sense that it has mutually exclusive alternatives. We  try and generate timetables using the different alternatives.
  */
-public interface UnknownP {
-
+public abstract class UnknownP implements  Iterable {
+    private ArrayList<ArrayList<Event>> listOfOptions = new ArrayList<>();
+    public String name;
+    public abstract boolean equals(Object obj);
+    public void add(ArrayList<Event> Section) {
+        listOfOptions.add(Section);
+    }
+    public String getName() {
+        return this.name;
+    }
+    public Iterator iterator() {
+        return new SectionIterator();
+    }
+    public class SectionIterator implements Iterator {
+        int cuurentSection = 0;
+        @Override
+        public boolean hasNext(){
+            return cuurentSection + 1 < listOfOptions.size();
+        }
+        @Override
+        public List<Event> next() {
+//            TODO implement DEEPclone
+            return listOfOptions.get(cuurentSection++);
+        }
+    }
 }
